87c47b20617de214f0cd186784a12a9214d332bc
Brian Hogan <brianhogan@napcs.com>
2022-07-05 16:31:16 -0500

    update links in footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -203,7 +203,7 @@ const config = {
             items: [
               {
                 label: "Join the Cloud Waitlist",
-                href: "https://us17.list-manage.com/survey?u=2334a0f23e55fd1840613755d&id=f1895b6f4a",
+                href: "https://pages.temporal.io/cloud-early-access",
               },
               {
                 label: "Subscribe to the Newsletter",
