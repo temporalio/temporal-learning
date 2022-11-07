41761650375da1aa611400f284d89a883c64184f
Cully Wakelin <cully@temporal.io>
2022-11-07 11:25:14 -0700

    adding algolia search to site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,6 +73,13 @@ const config = {
         autoCollapseCategories: true,
       },
     },
+    algolia: {
+      apiKey: "cd527863e60d95ebe650cdd21c7a6f3f",
+      indexName: "temporal",
+      // contextualSearch: true, // Optional, If you different version of docs etc (v1 and v2) doesn't display dup results
+      appId: "T5D6KNJCQS", // Optional, if you run the DocSearch crawler on your own
+      // algoliaOptions: {}, // Optional, if provided by Algolia
+    },
     navbar: {
       hideOnScroll: false,
       logo: {
