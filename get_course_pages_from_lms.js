5d226950c9cae5d23ad1fa8d5e0fb0c26991b56a
Tom Wheeler <tom@temporal.io>
2024-03-28 12:27:58 -0500

    updated to point to the new revision of Temporal 102 with Go (which has a different structure and eliminates content on Versioning as compared to the original revision)

--- get_course_pages_from_lms.js
+++ get_course_pages_from_lms.js
@@ -35,7 +35,7 @@ const courseData = [
     keywords: "[Temporal, Workflows, Activities, Python SDK, external service, recovery, execution model, event history, Temporal Web UI, command-line tools, business process, application lifecycle]",
     metaDescription: "Discover the essentials of Temporal application development in this course, focusing on Workflows, Activities, and the Python SDK. You'll develop a small app, recover from failures, and use Temporal's execution model and tools to manage your application lifecycle effectively."
   },
-  {code: '102_go', main: true, language: "Go", banner: "![Temporal Go SDK](/img/sdk_banners/banner_go.png)", filename: "temporal_102/go.md", index: 1,
+  {code: '102_go_r2', main: true, language: "Go", banner: "![Temporal Go SDK](/img/sdk_banners/banner_go.png)", filename: "temporal_102/go.md", index: 1,
     keywords: "[Temporal, application development, durable execution, development lifecycle, testing, debugging, deployment, best practices, automated testing, event history, workflow execution, production updates]",
     metaDescription: "Go beyond the basics and gain a deeper understand of how Temporal works as you explore Temporal's event history, application lifecycle, write tests, and explore Durable Execution.."
   },
