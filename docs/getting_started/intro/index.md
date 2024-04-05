0610cbcd54516e0c9627aa3a31d1f3d0535c0f9e
Brian Hogan <brianhogan@napcs.com>
2024-04-05 13:39:39 -0500

    h5p proof of concept

---
+++ docs/getting_started/intro/index.md
@@ -0,0 +1,13 @@
+---
+title: "Introducing Temporal"
+sidebar_position: 0
+sidebar_label: Intro
+hide_table_of_contents: false
+pagination_prev: null
+image: /img/temporal-logo-twitter-card.png
+---
+
+import { H5PEmbed } from '@site/src/components';
+
+<H5PEmbed h5pJsonPath="/h5p/intro" />
+
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as ResponsivePlayer} from "./responsive-player/ResponsivePlayer"
 export {default as Expander} from "./expander/Expander";
 export {Button} from "./shared/Button";
 export {default as OutdatedNotice} from "./outdated-notice/OutdatedNotice";
+export {default as H5PEmbed} from "./h5p/H5PEmbed";
