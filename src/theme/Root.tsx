b6634be91cb22356c871e6189e4af08091ba9074
David Carlson Berg <david.berg@temporal.io>
2026-09-15 12:05:03 -0400

    Add new line at end of file.

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,15 +1,15 @@
-import React, { type ReactNode } from 'react';
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import React, {type ReactNode} from "react";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
 if (ExecutionEnvironment.canUseDOM) {
-  import('@temporalio-web/consent-banner');
+  import("@temporalio-web/consent-banner");
 }
 
-export default function Root({ children }: { children: ReactNode }): ReactNode {
+export default function Root({children}: {children: ReactNode}): ReactNode {
   return (
     <>
       {children}
-      {React.createElement('temporal-consent-banner')}
+      {React.createElement("temporal-consent-banner")}
     </>
   );
 }
