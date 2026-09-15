4ad56f1bd9fd001dc2a09f21a479e26bd02006ab
David Carlson Berg <david.berg@temporal.io>
2026-09-15 11:39:31 -0400

    GRWMK-494: Add Consent Banner

---
+++ src/theme/Root.tsx
@@ -0,0 +1,15 @@
+import React, { type ReactNode } from 'react';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+
+if (ExecutionEnvironment.canUseDOM) {
+  import('@temporalio-web/consent-banner');
+}
+
+export default function Root({ children }: { children: ReactNode }): ReactNode {
+  return (
+    <>
+      {children}
+      {React.createElement('temporal-consent-banner')}
+    </>
+  );
+}
